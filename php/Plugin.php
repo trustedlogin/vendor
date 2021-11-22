3af63194961d62c0f24d95f1819b75bf7fe97244
Josh Pollock <jpollock412@gmail.com>
2021-11-22 14:30:42 -0500

    inline docs

--- php/Plugin.php
+++ php/Plugin.php
@@ -4,14 +4,23 @@ namespace TrustedLogin\Vendor;
 
 class Plugin
 {
-
+    /**
+     * @var Encryption $encryption
+     */
     protected Encryption $encryption;
 
-
+    /**
+     * @param Encryption $encryption
+     */
     public function __construct(Encryption $encryption){
         $this->encryption = $encryption;
     }
 
+    /**
+     * Add REST API endpoints
+     *
+     * @uses "rest_api_init" action
+     */
     public function restApiInit(){
         (new \TrustedLogin\Vendor\Endpoints\Settings())
             ->register();
@@ -21,11 +30,21 @@ class Plugin
             ->register();
     }
 
+    /**
+     * Get the encyption public key
+     *
+     * @return string
+     */
     public function getPublicKey(){
         return $this->encryption
             ->get_public_key();
     }
 
+    /**
+     * Get the encyption signature key
+     *
+     * @return string
+     */
     public function getSignatureKey(){
         return $this->encryption
             ->get_public_key( 'sign_public_key' );
