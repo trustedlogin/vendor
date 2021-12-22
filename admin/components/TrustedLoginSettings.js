3fe9d815a26dbfbafb998848a8fd34184f1058f9
Josh Pollock <jpollock412@gmail.com>
2021-12-21 20:23:47 -0500

    put team settings in boxes

--- admin/components/TrustedLoginSettings.js
+++ admin/components/TrustedLoginSettings.js
@@ -1,24 +1,36 @@
-import { Form, Submit, Input, FormTable } from "./index";
+import { Form, Submit, } from "./index";
 import { __ } from "@wordpress/i18n";
 import TeamSettings from "./TeamSettings";
+import {Metabox,MetaboxWrapper}from '@imaginary-machines/wp-admin-components'
 
 /**
  * TrustedLogin Settings Form
  */
 export default function ({ onSave, settings, canSave, setTeam }) {
+
 	return (
-		<Form onSubmit={onSave}>
-			{settings.teams
-				? settings.teams.map((team) => (
-						<TeamSettings team={team} setTeam={setTeam} key={team.id}  />
-				  ))
-				: null}
+		<>
+			<section>
+				<MetaboxWrapper>
+					{settings.teams
+						? settings.teams.map((team) => (
+							<Form onSubmit={onSave}>
+								<Metabox title={team.account_id} key={team.id}>
+									<TeamSettings team={team} setTeam={setTeam} key={team.id}  />
+								</Metabox>
+							</Form>
+						))
+						: null}
+				</MetaboxWrapper>
+			</section>
+
+
 			<Submit
 				onClick={onSave}
 				variant={canSave ? "primary" : "secondary"}
 				value={__("Save")}
 				disabled={!canSave}
 			/>
-		</Form>
+		</>
 	);
 }
