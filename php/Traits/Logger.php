f1a5f97fe0fe6ab16419e35e7f52546a26f6d638
Josh Pollock <jpollock412@gmail.com>
2022-01-12 17:50:26 -0500

    Improve logger more

--- php/Traits/Logger.php
+++ php/Traits/Logger.php
@@ -7,8 +7,9 @@ use DateTime;
 trait Logger
 {
 
-	public function log( $message,$method, $logLevel = 'info' ,array $context = [] )
+	public function log( $message,$method, $logLevel = 'info' , $context = [] )
 	{
+		$context = (array) $context;
 		$logLevel = strtolower( $logLevel );
 		if (defined('WP_DEBUG') && WP_DEBUG) {
 			error_log($message);
@@ -16,8 +17,12 @@ trait Logger
 		//2022/01/06 17:34:16 [notice]
 		$dateFormat = "Y/n/j g:i:a";
 		$dateTime = (new DateTime())->format($dateFormat);
-		//@todo changemode
-		$file = fopen(dirname(__FILE__, 3).'/trustedlogin.log', "a");
+		$logFileName = dirname(__FILE__, 3).'/trustedlogin.log';
+		if( ! file_exists( $logFileName ) ) {
+			touch( $logFileName );
+		}
+
+		$file = fopen($logFileName, "a");
 		$output = sprintf("%s [%s]: (%s) %s %s \n",
 			$dateTime,
 			$logLevel,
@@ -25,7 +30,6 @@ trait Logger
 			$message,
 			! empty( $context ) ? json_encode( $context ) : ''
 		);
-
 		fwrite($file, "\n". $output);
 		fclose($file);
 	}
