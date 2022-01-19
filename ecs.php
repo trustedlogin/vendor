ad2de3faf6bea3d1918b1ec9634b62d1faad4467
Josh Pollock <jpollock412@gmail.com>
2022-01-19 18:27:13 -0500

    Configure ecs

--- ecs.php
+++ ecs.php
@@ -1,16 +1,33 @@
 <?php
+
+declare(strict_types=1);
+
 use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+use PHP_CodeSniffer\Standards\Generic\Sniffs\Commenting\DocCommentSniff;
+use PHP_CodeSniffer\Standards\Generic\Sniffs\Commenting\ScopeIndent;
+
+
 return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::PATHS, [
+        __DIR__ . '/php',
+       // __DIR__ . '/tests',
+    ]);
 
-    // alternative to CLI arguments, easier to maintain and extend
-    $parameters->set(Option::PATHS, [__DIR__ . '/php', __DIR__ . '/tests']);
+    $services = $containerConfigurator->services();
+    $services->set(ArraySyntaxFixer::class)
+        ->call('configure', [[
+            'syntax' => 'short',
+        ]]);
 
-    // A. full sets
-    $containerConfigurator->import(SetList::PSR_12);
 
 
+
+    // $containerConfigurator->import(SetList::ARRAY);
+    // $containerConfigurator->import(SetList::DOCBLOCK);
+     $containerConfigurator->import(SetList::PSR_12);
 };
