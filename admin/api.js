b37d14f72f47646fcd771ce3fb0577ce06823aeb
Josh Pollock <jpollock412@gmail.com>
2022-02-07 13:20:58 -0500

    do some lints

--- admin/api.js
+++ admin/api.js
@@ -2,29 +2,29 @@ import apiFetch from "@wordpress/api-fetch";
 
 const path = "/trustedlogin/v1/settings";
 export const getSettings = async () => {
-	let settings = await apiFetch({ path }).catch( e => console.log(e));
-	if (settings.teams) {
-		settings.teams = settings.teams.map((team, id) => {
-			if( !team.helpdesk ) {
-				team.helpdesk = "helpscout";
-			}
+  let settings = await apiFetch({ path }).catch((e) => console.log(e));
+  if (settings.teams) {
+    settings.teams = settings.teams.map((team, id) => {
+      if (!team.helpdesk) {
+        team.helpdesk = "helpscout";
+      }
 
-			return {
-				id,
-				...team,
-			};
-		});
-	}
-	return settings;
+      return {
+        id,
+        ...team,
+      };
+    });
+  }
+  return settings;
 };
 
 export const updateSettings = async ({ teams }) => {
-	let data = await apiFetch({
-		path,
-		method: "POST",
-		data: {
-			teams,
-		},
-	});
-	return data;
+  let data = await apiFetch({
+    path,
+    method: "POST",
+    data: {
+      teams,
+    },
+  });
+  return data;
 };
