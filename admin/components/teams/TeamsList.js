083bcc86472fe474e3476c096b81628702cc01e2
Josh Pollock <jpollock412@gmail.com>
2022-02-22 18:14:12 -0500

    Another App test, more jsx fixes

--- admin/components/teams/TeamsList.js
+++ admin/components/teams/TeamsList.js
@@ -81,9 +81,9 @@ const TeamsList = () => {
                         fill="currentColor"
                         aria-hidden="true">
                         <path
-                          fill-rule="evenodd"
+                          fillRule="evenodd"
                           d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                          clip-rule="evenodd"
+                          clipRule="evenodd"
                         />
                       </svg>
                     </div>
@@ -155,9 +155,9 @@ const TeamsList = () => {
                               fill="currentColor"
                               aria-hidden="true">
                               <path
-                                fill-rule="evenodd"
+                                fillRule="evenodd"
                                 d="M5.293 9.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 7.414V15a1 1 0 11-2 0V7.414L6.707 9.707a1 1 0 01-1.414 0z"
-                                clip-rule="evenodd"
+                                clipRule="evenodd"
                               />
                             </svg>
                             <span className="sr-only">Increased by</span>
