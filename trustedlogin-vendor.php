8eb39affac1e32ba48caf45f1cae8d0321a2ce28
Josh Pollock <jpollock412@gmail.com>
2022-03-01 15:08:00 -0500

    Do not early return on action init

--- trustedlogin-vendor.php
+++ trustedlogin-vendor.php
@@ -48,14 +48,10 @@ if( file_exists( $path . 'vendor/autoload.php' ) ){
 
     //Add REST API endpoints
 	add_action( 'rest_api_init', [$plugin, 'restApiInit']);
-	return;
     //Handle access key login if requests.
 	add_action( 'template_redirect',[\TrustedLogin\Vendor\MaybeRedirect::class, 'handle']);
 	//Handle the "Reset All" button in UI
 	add_action( 'admin_init',[\TrustedLogin\Vendor\MaybeRedirect::class, 'adminInit']);
-
-	//Register error handler
-	//ErrorHandler::register();
 }else{
 	throw new \Exception('Autoloader not found.');
 }
