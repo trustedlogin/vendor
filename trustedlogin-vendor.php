46777cba4cadc08f8b4cfe528f70e912e7aa8846
Josh Pollock <jpollock412@gmail.com>
2022-05-11 12:37:25 -0400

    Set version 10.11

--- trustedlogin-vendor.php
+++ trustedlogin-vendor.php
@@ -3,7 +3,7 @@
  * Plugin Name: TrustedLogin Support Plugin
  * Plugin URI: https://www.trustedlogin.com
  * Description: Authenticate support team members to securely log them in to client sites via TrustedLogin
- * Version: 0.10.0
+ * Version: 0.11.0
  * Requires PHP: 7.1
  * Author: Katz Web Services, Inc.
  * Author URI: https://www.trustedlogin.com
@@ -20,7 +20,7 @@ if (!defined('ABSPATH')) {
 }
 // Exit if accessed directly
 
-define( 'TRUSTEDLOGIN_PLUGIN_VERSION', '0.10.0' );
+define( 'TRUSTEDLOGIN_PLUGIN_VERSION', '0.11.0' );
 define( 'TRUSTEDLOGIN_PLUGIN_FILE', __FILE__ );
 if( ! defined( 'TRUSTEDLOGIN_API_URL')){
 	define( 'TRUSTEDLOGIN_API_URL', 'https://app.trustedlogin.com/api/v1/' );
