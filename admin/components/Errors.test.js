983991a8be42233b109e16f00bc8adc8ebf80459
Josh Pollock <jpollock412@gmail.com>
2022-05-23 13:24:19 -0400

    Tests and fixes for ScreenError component #33

---
+++ admin/components/Errors.test.js
@@ -0,0 +1,56 @@
+import AccessKeyForm from "./AccessKeyForm";
+import { render, act, fireEvent } from "@testing-library/react";
+import { ScreenError } from "./Errors";
+describe("ScreenError", () => {
+  it("renders with only heading", () => {
+    const { container } = render(<ScreenError heading={"An Error"} />);
+    expect(container).toMatchSnapshot();
+  });
+  it("renders with text & heading", () => {
+    const { container } = render(
+      <ScreenError heading={"An Error"} text={"Some text"} />
+    );
+    expect(container).toMatchSnapshot();
+  });
+  it("is retryable", () => {
+    const { container, getByText } = render(
+      <ScreenError
+        isDismissible={true}
+        heading={"An Error"}
+        text={"Some text"}
+      />
+    );
+    expect(container.querySelectorAll(".tl-error").length).toBe(1);
+    act(() => {
+      fireEvent.click(getByText("Dismiss"));
+    });
+
+    expect(container.querySelectorAll(".tl-error").length).toBe(0);
+  });
+  it("Is retryable", () => {
+    const retryClick = jest.fn();
+    const { getByText } = render(<ScreenError retryClick={retryClick} />);
+    act(() => {
+      fireEvent.click(getByText("Try again"));
+    });
+    expect(retryClick).toHaveBeenCalledTimes(1);
+  });
+  it("Has learn more link", () => {
+    const learnMoreLink = "https://example.com/";
+    const { getByText } = render(<ScreenError learnMoreLink={learnMoreLink} />);
+
+    expect(getByText("Learn more").href).toEqual(learnMoreLink);
+  });
+  it("Has learn more link && is retryable", () => {
+    const learnMoreLink = "https://example.com/";
+    const retryClick = jest.fn();
+    const { getByText } = render(
+      <ScreenError learnMoreLink={learnMoreLink} retryClick={retryClick} />
+    );
+    expect(getByText("Learn more").href).toEqual(learnMoreLink);
+    act(() => {
+      fireEvent.click(getByText("Try again"));
+    });
+    expect(retryClick).toHaveBeenCalledTimes(1);
+  });
+});
